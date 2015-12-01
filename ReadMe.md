040a7f4d3b3cf452628be02ab9c7b8aa72053ab5
Robert Hill <codingbyrobert@gmail.com>
2015-11-30 22:15:59 -0800

    sorting working

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+Jesse Touch and I did some pair programing to get the basic stuff working.
+Used info posted in slack(stackoverflow) for the date function.
--- scripts/article.js
+++ scripts/article.js
@@ -14,22 +14,28 @@ var Article = function(props){
   this.blogTitle = props.blogTitle;
   this.blogBody = props.blogBody;
   this.publishedOn = props.publishedOn;
+  this.category = props.category;
 }
 
 Article.prototype.toHTML = function(){
   var $artClone = $('#template').clone();
   // console.log($artClone);
   $artClone.find('.blogTitle').html(this.blogTitle);
-  $artClone.find('.author').html('by ' +'<a href ='+'"'+ this.authorUrl+'"' +'>' + this.author+'</a> ' + daysSincePost(this.publishedOn) +' days since posted');
+  $artClone.find('.author').html('A post about ' + this.category +' by ' +'<a href ='+'"'+ this.authorUrl+'"' +'>' + this.author+'</a> ' + daysSincePost(this.publishedOn) +' days since posted');
   $artClone.find('.blogBody').html(this.blogBody);
   daysSincePost(this.publishedOn);
   $("main").append($artClone);
 }
 
-
-
-for (var i = 0; i < blog.rawData.length; i++){
-  var art = new Article(blog.rawData[i]);
-  art.toHTML();
- // console.log(blog.rawData[i].blogTitle);
+function render(){
+  blog.rawData.sort(function(a, b) {
+    a = new Date(a.publishedOn);
+    b = new Date(b.publishedOn);
+    return a>b ? -1 : a<b ? 1 : 0;
+  });
+  for (var i = 0; i < blog.rawData.length; i++){
+      var art = new Article(blog.rawData[i]);
+      art.toHTML();
+    }
 }
+render();
