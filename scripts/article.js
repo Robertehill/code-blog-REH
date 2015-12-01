24c04e4afdea0578145fc68e68385141a1d47adc
Robert Hill <codingbyrobert@gmail.com>
2015-11-30 17:48:08 -0800

    basics working

--- scripts/article.js
+++ scripts/article.js
@@ -1,10 +1,22 @@
 var Article = function(props){
   this.author = props.author;
+  this.authorUrl = props.authorUrl;
   this.blogTitle = props.blogTitle;
   this.blogBody = props.blogBody;
   this.publishDate = props.publishDate;
 }
 
-Article.prototype.toHTML = function(first_argument){
-  return '<article>'+'<h1>'+ this.blogTitle +'</h1>'+'</article>'
+Article.prototype.toHTML = function(){
+  var $artClone = $('#template').clone();
+  // console.log($artClone);
+  $artClone.find('.blogTitle').html(this.blogTitle);
+  $artClone.find('.author').html('by ' + this.author + ' on ' + this.publishDate);
+  // $artClone.find('.author').html(this.author);
+
+  $("main").append($artClone);
+}
+for (var i = 0; i < blog.rawData.length; i++){
+  var art = new Article(blog.rawData[i]);
+  art.toHTML();
+ // console.log(blog.rawData[i].blogTitle);
 }
--- scripts/blog.js
+++ scripts/blog.js
@@ -1,2 +1,1 @@
 var blog = {};
-blog.articles = [];
