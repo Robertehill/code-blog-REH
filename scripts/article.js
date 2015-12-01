abf36973352f22cdb29aad33a822a5d418b68bc8
Robert Hill <codingbyrobert@gmail.com>
2015-12-01 13:31:28 -0800

    moved functions to blog{}

--- scripts/article.js
+++ scripts/article.js
@@ -1,13 +1,3 @@
-function daysSincePost(postDate){
-  var today = new Date();
-  var oneDay = 24*60*60*1000;
-  var postDate = new Date(postDate);
-  var currentDate = today;
-  // console.log(today);
-  // console.log(postDate);
-  return Math.round(Math.abs((postDate.getTime()-currentDate.getTime())/(oneDay)));
-}
-
 var Article = function(props){
   this.author = props.author;
   this.authorUrl = props.authorUrl;
@@ -16,26 +6,16 @@ var Article = function(props){
   this.publishedOn = props.publishedOn;
   this.category = props.category;
 };
-
 Article.prototype.toHTML = function(){
   var $artClone = $('#template').clone();
-  // console.log($artClone);
-  $artClone.find('.blogTitle').html(this.blogTitle);
-  $artClone.find('.author').html('A post about ' + this.category +' by ' +'<a href ='+'"'+ this.authorUrl+'"' +'>' + this.author+'</a> ' + daysSincePost(this.publishedOn) +' days since posted');
+  $artClone.removeAttr('id');
+  $artClone.attr('id', this.blogTitle);
+  $artClone.find('.blogTitle').text(this.blogTitle);
+  $artClone.find('.authorLink').attr('href', this.authorUrl).text('By ' + this.author);
+  $artClone.find('.author').after('A post about ' + this.category + ' that is ' +blog.daysSincePost(this.publishedOn) +' days old');
+
   $artClone.find('.blogBody').html(this.blogBody);
-  daysSincePost(this.publishedOn);
   $('main').append($artClone);
 };
-
-function render(){
-  blog.rawData.sort(function(a, b) {
-    a = new Date(a.publishedOn);
-    b = new Date(b.publishedOn);
-    return a>b ? -1 : a<b ? 1 : 0;
-  });
-  for (var i = 0; i < blog.rawData.length; i++){
-    var art = new Article(blog.rawData[i]);
-    art.toHTML();
-  }
-}
-render();
+$(document).ready(blog.render());
+// blog.render(); //calling this here, this is the last line that loads(at this time)
--- scripts/blog.js
+++ scripts/blog.js
@@ -1,1 +1,18 @@
 var blog = {};
+blog.render = function(){
+  blog.rawData.sort(function(a, b) {
+    a = new Date(a.publishedOn);
+    b = new Date(b.publishedOn);
+    return a>b ? -1 : a<b ? 1 : 0;
+  });
+  for (var i = 0; i < blog.rawData.length; i++){
+    var art = new Article(blog.rawData[i]);
+    art.toHTML();
+  }
+};
+blog.daysSincePost = function(postDate){
+  var today = new Date();
+  var oneDay = 24*60*60*1000;
+  var postDate = new Date(postDate);
+  return Math.round(Math.abs((postDate.getTime()-today.getTime())/(oneDay)));
+};
