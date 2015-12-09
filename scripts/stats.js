2ad4c6840f6fbd62a4642612dd0faf558e5a839c
Robert Hill <codingbyrobert@gmail.com>
2015-12-08 16:28:26 -0800

    number of articles and authors working on stats.html

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,4 +1,38 @@
 var stats = {};
-stats.numOfAuthors = [];
 stats.numOfArticles = [];
 stats.numOfWords = [];
+
+stats.getArrayLength = function(array){
+  return array.length;
+};
+// stats.getNumOfAuthors = function(array){
+//   var numOfAuthors = [];
+//
+//   // if (numOfAuthors.indexOf(_.map(array, 'author')) < 0){
+//   // }
+//   return numOfAuthors.length;
+// };
+stats.getNumOfAuthors = function(items, prop) {
+  var unique = [];
+  var numOfAuthors = [];
+
+  _.each(items, function(item) {
+    if (unique[item[prop]] === undefined) {
+      numOfAuthors.push(item);
+    }
+
+    unique[item[prop]] = 0;
+  });
+
+  return numOfAuthors.length;
+};
+console.log(stats.getNumOfAuthors(blog.rawData, 'author'));
+
+stats.toHtml = function(text, value){
+  $('#blog-stats-section').append(text + ' : ' + value + '<br />');
+};
+$(function(){
+  stats.toHtml('Number of Articles', stats.getArrayLength(blog.rawData));
+  stats.toHtml('Number of Authors', stats.getNumOfAuthors(blog.rawData, 'author'));
+
+})
