a67639dd82103294e40a78d22fa5870ae682a718
Robert Hill <codingbyrobert@gmail.com>
2015-12-08 18:03:47 -0800

    total words working

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,38 +1,43 @@
 var stats = {};
-stats.numOfArticles = [];
-stats.numOfWords = [];
 
 stats.getArrayLength = function(array){
   return array.length;
 };
-// stats.getNumOfAuthors = function(array){
-//   var numOfAuthors = [];
-//
-//   // if (numOfAuthors.indexOf(_.map(array, 'author')) < 0){
-//   // }
-//   return numOfAuthors.length;
-// };
-stats.getNumOfAuthors = function(items, prop) {
+stats.getNumOfProp = function(items, prop) {
   var unique = [];
-  var numOfAuthors = [];
-
+  var numOfProp = [];
   _.each(items, function(item) {
     if (unique[item[prop]] === undefined) {
-      numOfAuthors.push(item);
+      numOfProp.push(item);
     }
-
     unique[item[prop]] = 0;
   });
-
-  return numOfAuthors.length;
+  return numOfProp.length;
 };
-console.log(stats.getNumOfAuthors(blog.rawData, 'author'));
+stats.wordCountPerArtilce = [];
+stats.countAll = function() {
+  blog.rawData.forEach(function(article) {
+    stats.wordCountPerArtilce.push(stats.countArticle(article));
+  });
+};
+stats.countArticle = function(article) {
+  return article.blogBody.split(' ').length;
+};
+stats.countAll();
+var totalWords = _.reduce(stats.wordCountPerArtilce, function(total, n) {
+  return total + n;
+});
+
+console.log(stats.wordCountPerArtilce);
+
 
 stats.toHtml = function(text, value){
   $('#blog-stats-section').append(text + ' : ' + value + '<br />');
 };
 $(function(){
-  stats.toHtml('Number of Articles', stats.getArrayLength(blog.rawData));
-  stats.toHtml('Number of Authors', stats.getNumOfAuthors(blog.rawData, 'author'));
 
-})
+  stats.toHtml('Number of Articles', stats.getArrayLength(blog.rawData));
+  stats.toHtml('Number of Authors', stats.getNumOfProp(blog.rawData, 'author'));
+  stats.toHtml('Number of Categories' , stats.getNumOfProp(blog.rawData, 'category'));
+  stats.toHtml('Number of Words', totalWords);
+});
