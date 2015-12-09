6c34949f54814f20426e2ee4390d43faeb351387
Robert Hill <codingbyrobert@gmail.com>
2015-12-08 21:17:00 -0800

    total word filter improved

--- scripts/stats.js
+++ scripts/stats.js
@@ -17,27 +17,39 @@ stats.getNumOfProp = function(items, prop) {
 stats.wordCountPerArtilce = [];
 stats.countAll = function() {
   blog.rawData.forEach(function(article) {
-    stats.wordCountPerArtilce.push(stats.countArticle(article));
+    stats.wordCountPerArtilce.push(stats.countWordsPerArticle(article));
   });
 };
-stats.countArticle = function(article) {
-  return article.blogBody.split(' ').length;
+stats.excludeAttr = function(segment) {
+  return !segment.startsWith('<img')
+  && !segment.startsWith('src=')
+  && !segment.startsWith('class=')
+  && !segment.startsWith('<')
+  && !segment.startsWith('com/')
+  && !segment.startsWith('"http')
+  && segment !== ("");
+};
+stats.countWordsPerArticle = function(article) {
+  console.log(article.blogBody.split(/\s|\.|\>|\;|=/).filter(stats.excludeAttr));
+  return article.blogBody.split(' ').filter(stats.excludeAttr).length;
 };
 stats.countAll();
-var totalWords = _.reduce(stats.wordCountPerArtilce, function(total, n) {
+stats.totalWords = _.reduce(stats.wordCountPerArtilce, function(total, n) {
   return total + n;
 });
-
-console.log(stats.wordCountPerArtilce);
-
-
+stats.avgWordsPerArt = function(array) {
+  return Math.round(stats.totalWords / stats.getArrayLength(array));
+};
+stats.avgWordLength = function(argument) {
+  // here for future use
+}
 stats.toHtml = function(text, value){
   $('#blog-stats-section').append(text + ' : ' + value + '<br />');
 };
 $(function(){
-
   stats.toHtml('Number of Articles', stats.getArrayLength(blog.rawData));
   stats.toHtml('Number of Authors', stats.getNumOfProp(blog.rawData, 'author'));
   stats.toHtml('Number of Categories' , stats.getNumOfProp(blog.rawData, 'category'));
-  stats.toHtml('Number of Words', totalWords);
+  stats.toHtml('Number of Words', stats.totalWords);
+  stats.toHtml('Average words per Article', stats.avgWordsPerArt(blog.rawData));
 });
